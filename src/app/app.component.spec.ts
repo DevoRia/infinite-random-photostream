fe2c0208e698f0b5d17f0785f6ccf6d36c36b5e3
Vadym Riabchenko <Vadym.Riabchenko@externals.adidas.com>
2023-03-16 09:53:28 +0200

    add scaffolding

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'XM-Test'`, () => {
+  it(`should have as title 'XM-test'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('XM-Test');
+    expect(app.title).toEqual('XM-test');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('XM-Test app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('XM-test app is running!');
   });
 });
